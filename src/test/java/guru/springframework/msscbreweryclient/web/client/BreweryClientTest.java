9c44cc8a3694fc8ccdf850ec20f663dea25e5cc5
Jesús Sánchez Bertolín <jsbertolin@gmail.com>
2021-01-23 20:22:27 +0100

    add GET and POST methods to the client app + tests

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void testGetBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = breweryClient.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+}
